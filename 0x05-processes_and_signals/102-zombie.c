0892f41375c23b6ba7d03bc3e7936e424f356eb8
ismail-la <lahbariismail@gmail.com>
2023-07-28 17:55:23 -0700

    0x05. Processes and signals

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,4 +1,4 @@
-/* C program that creates 5 zombie processes */
+/* C program that creates five zombie processes */
 
 /*
  * File: 102-zombie.c
@@ -35,22 +35,17 @@ int infinite_while(void)
 
 int main(void)
 {
-	char count = 0;
-	pid_t pid;
+	pid_t zombie;
+	int i;
 
-	while (count < 5)
+	for (i = 0; i < 5; i++)
 	{
-		pid = fork();
-		if (pid > 0)
-		{
-			printf("Zombie process created, PID: %d\n", pid);
-			sleep(1);
-			count++;
-		}
-		else
-			exit(0);
+		zombie = fork();
+		if (!zombie)
+			return (0);
+		printf("Zombie process created, PID: %d\n", zombie);
 	}
 
 	infinite_while();
-	return (EXIT_SUCCESS);
+	return (0);
 }
