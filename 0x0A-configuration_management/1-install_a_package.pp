66ed077018280f084c7797dead120803af322618
ismail-la <lahbariismail@gmail.com>
2023-09-22 16:30:59 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Using Puppet, install flask from pip3.
 # Requirements:
-   # Install flask
-   # Version must be 2.1.0
+#  Install flask
+#  Version must be 2.1.0
 
 package { 'flask':
   ensure   => '2.1.0',
