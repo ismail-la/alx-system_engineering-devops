5317dc8907ac83efa1bc7ad548a21993b9378854
ismail-la <lahbariismail@gmail.com>
2023-09-24 17:25:44 -0700

    0x0B. SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# set up your client SSH configuration file so that you can connect
+# to a server without typing a password.
+# Puppet script to create ssh config file
+# Requirements:
+#   Your SSH client configuration must be configured to use the private key ~/.ssh/school
+#   Your SSH client configuration must be configured to refuse to authenticate using a password
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
