720c1522499cc621d9a9a1468a731bf1c05b3f97
ismail-la <lahbariismail@gmail.com>
2023-09-26 19:40:33 -0700

    0x0C. Web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,38 @@
+# Script to Install nginx web server and configure using puppet
+
+include stdlib
+
+# Update apt
+
+exec { 'Update apt library':
+  command => 'sudo apt update',
+  path    => '/usr/bin/'
+}
+
+# Install nginx
+
+package { 'nginx':
+  ensure   => 'installed',
+}
+
+# Configure nginx
+
+file_line { 'Add redirect line':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'server_name _;',
+  line   => "location /redirect_me {\n\treturn 301 http://github.com/;\n}",
+}
+
+# Set index content
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Restart nginx service
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
