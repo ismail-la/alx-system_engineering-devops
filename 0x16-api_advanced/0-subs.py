750f17c4db8383a87b319d3ec0eee621f59020a0
ismail-la <lahbariismail@gmail.com>
2023-11-08 01:48:02 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,29 +10,29 @@ from requests import get
 
 def number_of_subscribers(subreddit):
     """
-	defines a function that takes a subreddit name as an argument.
-	If an invalid subreddit is given, the function should return 0.
+    defines a function that takes a subreddit name as an argument.
+    If an invalid subreddit is given, the function should return 0.
     """
 
-	# checks if the input is None or not a string
+    # checks if the input is None or not a string
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-	#  sets the user agent for the HTTP request.
+    #  sets the user agent for the HTTP request.
     headers = {'User-agent': 'My browser'}
 
-	# formats the URL to fetch data about the subreddit.
+    # formats the URL to fetch data about the subreddit.
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
-	# sends a GET request to the URL with the specified user agent.
+    # sends a GET request to the URL with the specified user agent.
     response = get(url, headers=headers)
 
-	# converts the response from the GET request into JSON format.
+    # converts the response from the GET request into JSON format.
     result = response.json()
 
-	# tries to return the number of subscribers from the JSON data.
+    # tries to return the number of subscribers from the JSON data.
     try:
         return result.get('data').get('subscribers')
 
-	# If there’s an error (like the subreddit doesn’t exist), it returns 0.
+    # If there’s an error (like the subreddit doesn’t exist), it returns 0.
     except Exception:
         return 0
