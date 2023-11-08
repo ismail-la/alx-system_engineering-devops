88d7a279d5bf009c918e2460fc4d64ded363cd11
ismail-la <lahbariismail@gmail.com>
2023-11-08 02:11:41 +0100

    update

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Recursive function that queries the Reddit API and
+returns a list containing the titles of all hot articles for a given subreddit.
+"""
+import requests
+
+
+def recurse(subreddit, after=None):
+    """
+    method doc
+    returning top ten post titles recursively
+    """
+    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+    parameters = {"after": after}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False,
+                            headers=headers, params=parameters)
+    all_posts = []
+    if response.status_code == 200:
+        data = response.json()
+        after_data = data["data"]["after"]
+        if after_data is None:
+            return all_posts
+        for post in data["data"]["children"]:
+            all_posts.append(post["data"]["title"])
+        next = recurse(subreddit, after_data)
+        all_posts.extend(next)
+        return all_posts
+    return None
