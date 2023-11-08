08407dd4d8f192c005096d06e477d91c9356c413
ismail-la <lahbariismail@gmail.com>
2023-11-08 02:35:25 +0100

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,60 +2,48 @@
 """ raddit api
 Script to count subreddit titles with keywords
 """
-
 import requests
-import json
 
 
-def count_words(subreddit, word_list, after="", count=[]):
-    """count all words.
-    Args:
-    subreddit(str): The subreddit to search.
-    word_list(list): The list of words to search for in post titles.
-    instances(obj): Key/value pairs of words/counts.
-    after(str): The parameter for the next page of the API results.
-    count(int): The parameter of results matched thus far.
+def count_words(subreddit, word_list, instance={}, after="", count=0):
+    """Get count of subreddit titles with keywords.
     """
-    if after == "":
-        count = [0] * len(word_list)
+    # Get data
+    response = requests.request(
+        'GET',
+        'https://www.reddit.com/r/{}/hot.json?limit=100&after={}&count={}'.
+        format(subreddit, after, count),
+        headers={
+            'User-Agent': 'my browser'
+        },
+        allow_redirects=False
+    )
+    try:
+        result = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    request = requests.get(url,
-                           params={'after': after},
-                           allow_redirects=False,
-                           headers={'user-agent': 'My browser'})
+    result = result.get("data")
+    after = result.get("after")
+    count += result.get("dist")
+    for i in result.get("children"):
+        title = i.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                time = len([t for t in title if t == word.lower()])
+                if instance.get(word) is None:
+                    instance[word] = time
+                else:
+                    instance[word] += time
 
-    if request.status_code == 200:
-        results = request.json()
-
-        for topic in (results['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
-
-        after = results['data']['after']
-        if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
-
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        a = count[i]
-                        count[i] = count[j]
-                        count[j] = a
-                        a = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = a
-            for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
-        else:
-            count_words(subreddit, word_list, after, count)
+    if after is None:
+        if len(instance) == 0:
+            print("")
+            return
+        instances = sorted(instance.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instance]
+    else:
+        count_words(subreddit, word_list, instance, after, count)
