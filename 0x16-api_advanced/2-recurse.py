cd76b2654a82d0c95b90cbfbda40439149e5e75f
ismail-la <lahbariismail@gmail.com>
2023-11-08 02:13:52 +0100

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -19,12 +19,12 @@ def recurse(subreddit, after=None):
     all_posts = []
     if response.status_code == 200:
         data = response.json()
-        after_data = data["data"]["after"]
-        if after_data is None:
+        after = data["data"]["after"]
+        if after is None:
             return all_posts
         for post in data["data"]["children"]:
             all_posts.append(post["data"]["title"])
-        next = recurse(subreddit, after_data)
+        next = recurse(subreddit, after)
         all_posts.extend(next)
         return all_posts
     return None
