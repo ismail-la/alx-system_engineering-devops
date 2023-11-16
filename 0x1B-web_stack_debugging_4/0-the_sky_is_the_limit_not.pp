9b79c6f972e866eed4b5e1e9fccf9d9712366583
ismail-la <lahbariismail@gmail.com>
2023-11-16 18:49:19 +0100

    0x1B. Web stack debugging #4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,7 +4,7 @@
 
 # Increase the ULIMIT of the default file.
 exec { 'Set nginx_limit':
-  command => 'sudo sed -i "s/15/4096/g" /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
